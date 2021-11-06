66b8ce80214dd89a71180f033c47bae63db12aa5
Hugo Gomes <hugoasgomes@hotmail.com>
2021-11-06 23:39:30 +0000

    Draft implementation of the main screen (mobile)

--- birdy_mobile/lib/main.dart
+++ birdy_mobile/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'res/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,36 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Birdy',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: CustomColors.black,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Birdy'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +31,92 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedMenuItem = 0;
 
-  void _incrementCounter() {
+  void _setSelectedMenuItem(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedMenuItem = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.transparent,
+        title: Text(
+          widget.title,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 24.0,
+            fontWeight: FontWeight.w800
+          )),
+        bottomOpacity: 0.0,
+        elevation: 0.0,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 50.0,
+              height: MediaQuery.of(context).size.width - 50.0,
+              padding: const EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                border: Border.all(
+                  width: 6.0,
+                  color: Colors.black,
+                ),
+                borderRadius: BorderRadius.circular(600.0),
+              ),
+              child: MaterialButton(
+                color: Colors.black,
+                shape: const CircleBorder(),
+                onPressed: () {},
+                child: Center(
+                    child: Container(
+                      margin: const EdgeInsets.all(70.0),
+                      width: 100.0,
+                      height: 100.0,
+                      child: SvgPicture.asset(
+                        'assets/img/music-alt.svg',
+                        color: Colors.white,
+                        semanticsLabel: 'A red up arrow'
+                      )
+                    ),
+                  )
+              )
+            )
+            
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+                const AssetImage("assets/img/headphones.png"),
+                color: _selectedMenuItem == 0 ? Colors.white : Colors.white54,
+                size: 24,
+            ),
+            label: 'Listen',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+                const AssetImage("assets/img/history-icon.png"),
+                color: _selectedMenuItem == 1 ? Colors.white : Colors.white54,
+                size: 24,
+            ),
+            label: 'History',
+          ),
+        ],
+        currentIndex: _selectedMenuItem,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white54,
+        onTap: _setSelectedMenuItem,
+      ),
     );
   }
 }
---
+++ birdy_mobile/lib/res/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Map<int, Color> black_code = {
+    50: Color.fromRGBO(0, 0, 0, .1),
+    100: Color.fromRGBO(0, 0, 0, .2),
+    200: Color.fromRGBO(0, 0, 0, .3),
+    300: Color.fromRGBO(0, 0, 0, .4),
+    400: Color.fromRGBO(0, 0, 0, .5),
+    500: Color.fromRGBO(0, 0, 0, .6),
+    600: Color.fromRGBO(0, 0, 0, .7),
+    700: Color.fromRGBO(0, 0, 0, .8),
+    800: Color.fromRGBO(0, 0, 0, .9),
+    900: Color.fromRGBO(0, 0, 0, 1),
+  };
+  static const MaterialColor black = MaterialColor(0xFF000000, black_code);
+}
