f57b6d22a6e1e1cde96ad6272aa3fef16a0285ac
Hugo Gomes <hugoasgomes@hotmail.com>
2021-11-07 19:19:25 +0000

    Experiment with audio recording and playback

--- birdy_mobile/lib/views/listen_view.dart
+++ birdy_mobile/lib/views/listen_view.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:record/record.dart';
 
 class ListenView extends StatefulWidget {
   const ListenView({Key? key}) : super(key: key);
@@ -9,6 +15,57 @@ class ListenView extends StatefulWidget {
 }
 
 class _ListenViewState extends State<ListenView> {
+  final Record _audioRecorder = Record();
+
+  void _recordAudio() async {
+    Map<Permission, PermissionStatus> permissions = await [
+      Permission.storage,
+      Permission.microphone,
+    ].request();
+
+    bool permissionsGranted = permissions[Permission.storage]!.isGranted &&
+        permissions[Permission.microphone]!.isGranted;
+    String rootDir = (await getApplicationDocumentsDirectory()).path;
+    if (permissionsGranted) {
+      Directory appFolder = Directory(rootDir);
+      bool appFolderExists = await appFolder.exists();
+      if (!appFolderExists) {
+        final created = await appFolder.create(recursive: true);
+        // print(created.path);
+      }
+
+      final filepath = rootDir + DateTime.now().millisecondsSinceEpoch.toString() + '.wav';
+
+      await _audioRecorder.start(path: filepath);
+      print('Starting... ' + filepath);
+
+      sleep(const Duration(seconds: 6));
+      stopRecording();
+      // emit(RecordOn());
+    } else {
+      print('Permissions not granted');
+    }
+  }
+
+  void stopRecording() async {
+    try {
+      String? path = await _audioRecorder.stop();
+      // emit(RecordStopped());
+      print('Output path $path');
+
+      AudioPlayer _audioPlayer = AudioPlayer();
+      final duration = await _audioPlayer.setFilePath(path!);
+      print("DURATION: " + duration.toString());
+      await _audioPlayer.play();
+      sleep(const Duration(seconds: 4));
+
+      await _audioPlayer.stop();
+
+    } catch(e) {
+      print("Error: " + e.toString());
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +88,7 @@ class _ListenViewState extends State<ListenView> {
             child: MaterialButton(
               color: Colors.black,
               shape: const CircleBorder(),
-              onPressed: () {},
+              onPressed: _recordAudio,
               child: Center(
                   child: Container(
                     margin: const EdgeInsets.all(70.0),
@@ -46,7 +103,6 @@ class _ListenViewState extends State<ListenView> {
                 )
             )
           )
-          
         ],
       ),
     );
